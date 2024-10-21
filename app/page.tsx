e0c53c5ca74ab8aa41120ae69920166265c96d61
davidfoo007 <tech@myunilah.com>
2024-10-21 20:12:14 +0800

    testimonial exp ctt

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,25 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/components/data";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { motion, useScroll } from "framer-motion";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     // const { scrollYProgress } = useScroll();
 
     return (
-        <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <main className="relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5">
             <div className="max-2-7xl w-full">
                 <FloatingNav navItems={navItems} />
-               
-                    <Hero />
-              
+                <Hero />
                 <Grid />
                 <RecentProjects />
+                <Clients />
+                <Experience />
+                <Approach />
+                <Footer />
             </div>
         </main>
     );
---
+++ components/Clients.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "./ui/Variants";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { companies, testimonials } from "./data";
+
+const Clients = () => {
+    return (
+        <div className="py-20" id="testimonials">
+            <motion.h1
+                variants={fadeIn("down", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.7 }}
+                className="heading"
+            >
+                Kind words from <span className="text-purple">satisfied client</span>
+            </motion.h1>
+            <div className="flex flex-wrap items-center justify-center max-lg mt-10">
+                <InfiniteMovingCards items={testimonials} direction="right" speed="slow" />
+                <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg mt-10">
+                    {companies.map(({ id, img, name, nameImg }) => (
+                        <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+                            <img src={img} alt={name} className="md:w-10 w-5" />
+                            <img src={nameImg} alt={name} className="md:w-24 w-20" />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Clients;
