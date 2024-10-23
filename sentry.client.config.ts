47988dbfdd1de2a313a724078889ef6fad8aafe8
davidfoo007 <tech@myunilah.com>
2024-10-24 01:07:26 +0800

    removed sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
     // Add optional integrations for additional features
 
     integrations: [
-        // Sentry.replayIntegration(),
+        Sentry.replayIntegration(),
         Sentry.feedbackIntegration({
             // Additional SDK configuration goes in here, for example:
             colorScheme: "system",
